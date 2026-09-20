07bcbb68fbae24b00daf89629ee1c189fca60fff
DK3628 <dk3628@srmist.edu.in>
2026-09-21 00:22:58 +0530

    Add files via upload

---
+++ src/main/java/oops&programmingconcept/assignment_problems/CanteenPOS.java
@@ -0,0 +1,43 @@
+class Payment {
+    public double pay(double amount) {
+        System.out.println("Paid (cash): Rs " + amount);
+        return amount;
+    }
+}
+
+class CardPayment extends Payment {
+    public double payWithProcessingFee(double amount) {
+        double totalCharged = amount * 1.02;
+        System.out.println("Charged (card, incl. fee): Rs " + totalCharged);
+        return totalCharged;
+    }
+}
+
+public class CanteenPOS {
+    public static double processTransaction(Payment payment, double amount) {
+        if (payment instanceof CardPayment) {
+            return ((CardPayment) payment).payWithProcessingFee(amount);
+        } else {
+            return payment.pay(amount);
+        }
+    }
+
+    public static void main(String[] args) {
+        Payment[] payments = {
+            new CardPayment(),
+            new Payment(),
+            new CardPayment(),
+            new Payment(),
+            new CardPayment()
+        };
+        double[] amounts = {100, 50, 200, 75, 120};
+        
+        double totalCollected = 0;
+
+        for (int i = 0; i < payments.length; i++) {
+            totalCollected += processTransaction(payments[i], amounts[i]);
+        }
+
+        System.out.println("Total Collected: Rs " + totalCollected);
+    }
+}
